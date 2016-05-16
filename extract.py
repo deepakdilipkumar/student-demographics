145dd28f212c6ff5ddcd00ae5ebc7911ce3fbd00
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-05-16 23:22:41 +0530

    Corrected issues with erratic pincode format

--- extract.py
+++ extract.py
@@ -1,25 +1,40 @@
 import re
+import csv
 
 with open("2012.txt") as f:
 	content=f.readlines()
 
-rollNo=[]
-pincode=[]
-regexRollNumber = re.compile("[0-9]{2}.[0-9]{6} -")
-regexPincode= re.compile("[0-9]{6}")
-regexMob = re.compile("[0-9]{10}")
+rollNo=[None]*600
+pincode=[None]*600
+regexRollNumber = re.compile("\D([0-9]{2}.[0-9]{6})\D")
+regexPincode= re.compile("\D([0-9]{6})\D")
+#regexMob = re.compile("[0-9]{10}")
+rollindex = 0
+pinindex = 0
+
 
 for line in content: 
-	if not regexMob.search(line):
-		if ( regexRollNumber.search(line)):  # Roll number
-			rollNo.append( regexRollNumber.search(line).group(0)  )
+	if ( regexRollNumber.search(line)):  # Roll number
+		if (rollindex > pinindex ):		 # Some pin code was missing
+			pinindex+=1
 
-		else:
-			if (regexPincode.search(line)):    #Pin Code
-				pincode.append( regexPincode.search(line).group(0) ) 
+		rollNo[rollindex] = regexRollNumber.findall(line)[0]
+		rollindex+=1
 
+
+	else:
+		if (regexPincode.search(line) and rollindex>pinindex):    #Pin Code, and to avoid pin code duplication
+			pincode[pinindex] = regexPincode.findall(line)[0] 
+			pinindex+=1
 
 print rollNo[len(rollNo)-1]
 print pincode[len(pincode)-1]
 print len(rollNo)
 print len(pincode)
+
+c = csv.writer(open("2012.csv", "wb"))
+c.writerow(["Roll Number","Pincode"])
+
+for i in range(len(rollNo)):
+	c.writerow([rollNo[i],pincode[i]])
+
