134b5e825245d15175eeacd96ad0154a421388f0
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-05-16 22:21:33 +0530

    Accounted for DD's

--- extract.py
+++ extract.py
@@ -5,11 +5,11 @@ with open("2012.txt") as f:
 
 rollNo=[]
 pincode=[]
-regexRollNumber= re.compile("[0-9]{9}")
+regexRollNumber = re.compile("[0-9]{2}.[0-9]{6}")
 regexPincode= re.compile("[0-9]{6}")
 
 for line in content: 
-	if ( regexRollNumber.search(line) ):  # Roll number
+	if ( regexRollNumber.search(line)):  # Roll number
 		rollNo.append( regexRollNumber.search(line).group(0)  )
 
 	else:
@@ -19,3 +19,5 @@ for line in content:
 
 print rollNo[len(rollNo)-1]
 print pincode[len(pincode)-1]
+print len(rollNo)
+print len(pincode)
