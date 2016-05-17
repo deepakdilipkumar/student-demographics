172fbecd00e6689b7e60b00e5a7ed73669085ffa
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-05-17 20:08:58 +0530

    Dynamic array size

--- extract.py
+++ extract.py
@@ -1,38 +1,44 @@
 import re
 import csv
 
-with open("2012.txt") as f:
+year = "2011"
+
+with open(year+".txt") as f:
 	content=f.readlines()
 
-rollNo=[None]*600
-pincode=[None]*600
+
+rollNo=[]
+pincode=[]
 regexRollNumber = re.compile("\D([0-9]{2}.[0-9]{6})\D")
 regexPincode= re.compile("\D([0-9]{6})\D")
 #regexMob = re.compile("[0-9]{10}")
 rollindex = 0
 pinindex = 0
-
+howmany=0
 
 for line in content: 
 	if ( regexRollNumber.search(line)):  # Roll number
 		if (rollindex > pinindex ):		 # Some pin code was missing
+			howmany+=1
 			pinindex+=1
+			pincode.append('')
 
-		rollNo[rollindex] = regexRollNumber.findall(line)[0]
+		#rollNo[rollindex] = regexRollNumber.findall(line)[0]
+		rollNo.append(regexRollNumber.findall(line)[0])
 		rollindex+=1
-
+		print rollindex
 
 	else:
 		if (regexPincode.search(line) and rollindex>pinindex):    #Pin Code, and to avoid pin code duplication
-			pincode[pinindex] = regexPincode.findall(line)[0] 
+			#pincode[pinindex] = regexPincode.findall(line)[0] 
+			pincode.append(regexPincode.findall(line)[0] )
 			pinindex+=1
 
-print rollNo[len(rollNo)-1]
-print pincode[len(pincode)-1]
 print len(rollNo)
 print len(pincode)
+print howmany
 
-c = csv.writer(open("2012.csv", "wb"))
+c = csv.writer(open(year+".csv", "wb"))
 c.writerow(["Roll Number","Pincode"])
 
 for i in range(len(rollNo)):
