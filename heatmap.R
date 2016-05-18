459e0fc21f0d8c3d656242a518ac91eced0b27cc
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-05-19 00:02:02 +0530

    Correcting lat/long import issues

--- heatmap.R
+++ heatmap.R
@@ -1,14 +1,21 @@
 library("ggmap")
 library("ggplot2")
 
-dfDemographics2011 <- csv.read("2011.csv", head=TRUE, as.is=TRUE)
-dfDemographics2012 <- csv.read("2012.csv", head=TRUE, as.is=TRUE)
-dfDemographics2013 <- csv.read("2013.csv", head=TRUE, as.is=TRUE)
-dfDemographics2014 <- csv.read("2014.csv", head=TRUE, as.is=TRUE)
+dfDemographics2011 <- read.csv("2011.csv", head=TRUE, as.is=TRUE)
+dfDemographics2012 <- read.csv("2012.csv", head=TRUE, as.is=TRUE)
+dfDemographics2013 <- read.csv("2013.csv", head=TRUE, as.is=TRUE)
+dfDemographics2014 <- read.csv("2014.csv", head=TRUE, as.is=TRUE)
 
 dfDemographics <- rbind(dfDemographics2011,dfDemographics2012,dfDemographics2013,dfDemographics2014)
+dfDemographics <- dfDemographics[dfDemographics$Pincode!="0",]
+
+
+latFactor <- factor(dfDemographics$Latitude)
+lonFactor <- factor(dfDemographics$Longitude)
+dfDemographics$Longitude <- as.numeric(levels(latFactor))[latFactor]
+dfDemographics$Latitude <- as.numeric(levels(lonFactor))[lonFactor]
 
 theme_set(theme_bw(16))
-indiaMap <- qmap("India", zoom = 8, color = "bw", legend = "topleft")
+indiaMap <- qmap("India", zoom = 5, legend = "topleft")
 indiaMap + 
-geom_point(aes(x = Longitude, y = Latitude, colour = Program), data = dfDemographics2012)
+geom_point(aes(x = Longitude, y = Latitude), data = dfDemographics[dfDemographics$Year=="2011",])
