87bb76fb0c84a227c07dd60a0263badbc7ec7cfd
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-05-21 22:46:47 +0530

    Adding district-wise grouping

---
+++ districtgrouping.R
@@ -0,0 +1,41 @@
+library("ggmap")
+library("ggplot2")
+
+dfDemographics2011 <- read.csv("2011.csv", head=TRUE, as.is=TRUE)
+dfDemographics2012 <- read.csv("2012.csv", head=TRUE, as.is=TRUE)
+dfDemographics2013 <- read.csv("2013.csv", head=TRUE, as.is=TRUE)
+dfDemographics2014 <- read.csv("2014.csv", head=TRUE, as.is=TRUE)
+
+dfDemographics <- rbind(dfDemographics2011,dfDemographics2012,dfDemographics2013,dfDemographics2014)
+dfDemographics <- dfDemographics[dfDemographics$Pincode!="0",]
+
+latFactor <- factor(dfDemographics$Latitude)
+lonFactor <- factor(dfDemographics$Longitude)
+dfDemographics$Latitude <- as.numeric(levels(latFactor))[latFactor]
+dfDemographics$Longitude <- as.numeric(levels(lonFactor))[lonFactor]
+
+District={}
+numStudents={}
+Longitude={}
+Latitude={}
+
+for (dist in levels(factor(dfDemographics2012$District))){
+	distStudents =  dfDemographics[dfDemographics$District==dist,]
+	numStudents=c(numStudents,length(distStudents$District))
+	District=c(District,dist)
+	Longitude = c(Longitude,mean(distStudents$Longitude))
+	Latitude = c(Latitude, mean(distStudents$Latitude))
+}
+
+dfDistrictDemographics2012 = data.frame(District,Longitude,Latitude, numStudents)
+
+
+
+# District wise grouping
+
+#theme_set(theme_bw(16))
+indiaMap <- qmap("India", zoom = 5, source = "google", maptype = "hybrid", legend = "topright") #terrain, satellite, roadmap, hybrid
+indiaMap + 
+geom_point(aes(x = Longitude, y = Latitude, size= numStudents, darken=0.5), data = dfDistrictDemographics2012)
+#facet_wrap(~ year)
+#+ stat_density2d(aes(x = Longitude, y = Latitude),size = 2, bins = 4, data = dfDemographics,geom = "polygon")
