375511f2d3fb1f3363124ac4a4df955baa2fc5b7
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-05-25 01:01:46 +0530

    Generalized year for heatmap, gradual gradient

--- heatmap.R
+++ heatmap.R
@@ -15,18 +15,21 @@ dfDemographics[dfDemographics$State=="CHATTISGARH","State"] <- "CHHATTISGARH"			
 dfDemographics[dfDemographics$State=="DELHI","State"] <- "NCT OF DELHI"
 dfDemographics[dfDemographics$State=="JAMMU & KASHMIR","State"] <- "JAMMU AND KASHMIR"
 
-dfDemographics2011 <- dfDemographics[dfDemographics$Year=="2011",]		# Update
-dfDemographics2012 <- dfDemographics[dfDemographics$Year=="2012",]
-dfDemographics2013 <- dfDemographics[dfDemographics$Year=="2013",]
-dfDemographics2014 <- dfDemographics[dfDemographics$Year=="2014",]
-
 latFactor <- factor(dfDemographics$Latitude)
 lonFactor <- factor(dfDemographics$Longitude)
 dfDemographics$Latitude <- as.numeric(levels(latFactor))[latFactor]		#Convert from character to numeric
 dfDemographics$Longitude <- as.numeric(levels(lonFactor))[lonFactor]
 
+dfDemographics2011 <- dfDemographics[dfDemographics$Year=="2011",]		# Update
+dfDemographics2012 <- dfDemographics[dfDemographics$Year=="2012",]
+dfDemographics2013 <- dfDemographics[dfDemographics$Year=="2013",]
+dfDemographics2014 <- dfDemographics[dfDemographics$Year=="2014",]
+
+year="2014"
+dfDemographicsBatch = dfDemographics[dfDemographics$Year==year,]
+
 relStudents={}
-statewise= table(dfDemographics2012$State)
+statewise= table(dfDemographicsBatch$State)
 for (state in names(statewise)) {
 	relStudents=c(relStudents,statewise[state]/max(statewise))			#Fraction of students relative to maximum
 }
@@ -46,9 +49,9 @@ for (id in indiaMapdf$OBJECTID){				# Loop over all the locations stored in indi
 extremeColors <- c("gray","black")			#Gradient to choose from
 pal <- colorRamp(extremeColors)
 pal2 <- colorRampPalette(extremeColors)
-pdf(file="statewise.pdf")
+pdf(file=paste("heatmap",year,".pdf"))
 plot(india, col=rgb(pal(locColor),maxColorValue=255))
-title(main="State Wise Distribution of 2012 IITB Entrants")
+title(main=paste("State Wise Distribution of",year,"IITB Entrants"))
 pnts = cbind(x =c(60,66,66,60), y =c(30,30,15,15))
-legend.gradient(pnts,col=pal2(5),limits=c(0,max(statewise)),title="Students")
+legend.gradient(pnts,col=pal2(1000),limits=c(0,max(statewise)),title="Students")
 dev.off()
