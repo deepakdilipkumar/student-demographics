38a1a34380db6899dffa83b6a4197b3d54162a24
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-06-05 21:35:18 +0530

    Separate scripts folder

---
+++ scripts/clean.py
@@ -0,0 +1,89 @@
+import csv 
+
+department=["","Aerospace","Chemical","Chemistry","Civil","Computer Science","","Electrical","","","Mechanical","Metallurgical","","","","","","Energy","","","","","","","","","Engineering Physics"]
+
+def location(pincode):
+	first = str(pincode)[0:2]
+	#island = str(pincode)[0:3]
+	#if island=="682":
+	#	return "Lakshwadeep"
+	#elif island =="744":
+	#	return "Andaman and Nicobar"
+	if first=="11":
+		return "Delhi"
+	elif first =="12" or first =="13":
+		return "Haryana"
+	elif first =="14" or first =="15" or first =="16":
+		return "Punjab"
+	elif first =="17":
+		return "Himachal Pradesh"
+	elif first =="18" or first =="19":
+		return "Jammu and Kashmir"
+	elif first >="20" and first <="28":
+		return "Uttar Pradesh"
+	elif first >="30" and first <="34":
+		return "Rajasthan"
+	elif first >="36" and first <="39":
+		return "Gujarat"
+	elif first >="40" and first <="44":
+		return "Maharashtra"
+	elif first >="45" and first <="48":
+		return "Madhya Pradesh"
+	elif first =="49":
+		return "Chhattisgarh"
+	elif first >="50" and first <="53":
+		return "Andhra Pradesh/Telangana"
+	elif first >="56" and first <="59":
+		return "Karnataka"
+	elif first >="60" and first <="64":
+		return "Tamil Nadu"
+	elif first >="67" and first <="69":
+		return "Kerala"
+	elif first >="70" and first <="74":
+		return "West Bengal"
+	elif first >="75" and first <="77":
+		return "Orissa"	
+	elif first =="78":
+		return "Assam"
+	elif first =="79":
+		return "North-Eastern States"
+	elif first >="84" and first <="85":
+		return "Bihar"
+	elif first >="80" and first <="83":
+		return "Bihar/Jharkhand"
+	elif first =="92":
+		return "Jharkhand"
+
+allPinCodes=[]
+allRollNos=[]
+depts=[]
+batch=[]
+prog=[]
+state=[]
+
+
+for year in ["2011","2012","2013","2014"]:
+	f= open(year+".csv","rb")
+	initialcsv = csv.reader(f)
+	for row in initialcsv:
+		rno = row[0]
+		pin = row[1]
+		allRollNos.append(rno)
+		allPinCodes.append(pin)
+		depts.append(department[int(str(rno)[3:5])])
+		if str(rno)[2] == "D":
+			prog.append("DD")
+		elif str(rno)[2] == "0":
+			prog.append("BTech")
+		elif str(rno)[2] == "1":
+			prog.append("MSc")
+		elif str(rno)[2] == "B":
+			prog.append("BS")
+		batch.append(year)
+		state.append(location(pin))
+
+c = csv.writer(open("demographics.csv", "wb"))
+c.writerow(["Roll Number","Batch","Department","Programme","Pincode","State"])
+
+for i in range(len(allRollNos)):
+	c.writerow([allRollNos[i],batch[i],depts[i],prog[i],allPinCodes[i],state[i]])
---
+++ scripts/heatmap.R
@@ -0,0 +1,57 @@
+library("ggmap")
+library("ggplot2")
+library("sp")
+library("SDMTools")
+
+dfDemographics2011 <- read.csv("2011.csv", head=TRUE, as.is=TRUE)
+dfDemographics2012 <- read.csv("2012.csv", head=TRUE, as.is=TRUE)
+dfDemographics2013 <- read.csv("2013.csv", head=TRUE, as.is=TRUE)
+dfDemographics2014 <- read.csv("2014.csv", head=TRUE, as.is=TRUE)
+
+dfDemographics <- rbind(dfDemographics2011,dfDemographics2012,dfDemographics2013,dfDemographics2014)
+dfDemographics <- dfDemographics[dfDemographics$Pincode!="0",]							# Remove entries without a pin code
+
+dfDemographics[dfDemographics$State=="CHATTISGARH","State"] <- "CHHATTISGARH"			# To match GADM data
+dfDemographics[dfDemographics$State=="DELHI","State"] <- "NCT OF DELHI"
+dfDemographics[dfDemographics$State=="JAMMU & KASHMIR","State"] <- "JAMMU AND KASHMIR"
+
+latFactor <- factor(dfDemographics$Latitude)
+lonFactor <- factor(dfDemographics$Longitude)
+dfDemographics$Latitude <- as.numeric(levels(latFactor))[latFactor]		#Convert from character to numeric
+dfDemographics$Longitude <- as.numeric(levels(lonFactor))[lonFactor]
+
+dfDemographics2011 <- dfDemographics[dfDemographics$Year=="2011",]		# Update
+dfDemographics2012 <- dfDemographics[dfDemographics$Year=="2012",]
+dfDemographics2013 <- dfDemographics[dfDemographics$Year=="2013",]
+dfDemographics2014 <- dfDemographics[dfDemographics$Year=="2014",]
+
+year="2014"
+dfDemographicsBatch = dfDemographics[dfDemographics$Year==year,]
+
+relStudents={}
+statewise= table(dfDemographicsBatch$State)
+for (state in names(statewise)) {
+	relStudents=c(relStudents,statewise[state]/max(statewise))			#Fraction of students relative to maximum
+}
+
+relStudentsdf = data.frame(names(statewise),relStudents)
+
+india <- readRDS("IND_adm1.rds")									#Read GADM data
+indiaMapdf <- india@data
+
+locColor=rep(0,length(indiaMapdf$OBJECTID))			
+for (id in indiaMapdf$OBJECTID){				# Loop over all the locations stored in indiaMapdf
+	if (length(relStudentsdf[relStudentsdf[,1]==toupper(indiaMapdf$NAME_1[id]),2])>0) {				# If the state corresponding to the location in indiaMapdf matches some state in relStudentsdf
+	locColor[id] <- relStudentsdf[relStudentsdf[,1]==toupper(indiaMapdf$NAME_1[id]),2]				# Assign the corresponding relative value for that state and store it in locColor
+}
+}
+
+extremeColors <- c("gray","black")			#Gradient to choose from
+pal <- colorRamp(extremeColors)
+pal2 <- colorRampPalette(extremeColors)
+pdf(file=paste("heatmap",year,".pdf"))
+plot(india, col=rgb(pal(locColor),maxColorValue=255))
+title(main=paste("State Wise Distribution of",year,"IITB Entrants"))
+pnts = cbind(x =c(60,66,66,60), y =c(30,30,15,15))
+legend.gradient(pnts,col=pal2(1000),limits=c(0,max(statewise)),title="Students")
+dev.off()
